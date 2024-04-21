65f16bd1b39c56f51ff4ceb7ae9bc84589a90183
liuyun <43500876@qq.com>
2024-04-21 13:17:59 +0800

    修改规格方法名称错误问题

--- erp/product/model_type.go
+++ erp/product/model_type.go
@@ -8,13 +8,13 @@ import (
 	Router2SystemConfig "github.com/fotomxq/weeekj_core/v5/router2/system_config"
 )
 
-// ArgsGetModelTypeList 获取品牌列表参数
+// ArgsGetModelTypeList 获取规格列表参数
 type ArgsGetModelTypeList struct {
 	//分页参数
 	Pages CoreSQL2.ArgsPages `json:"pages"`
 	//组织ID
 	OrgID int64 `db:"org_id" json:"orgID" check:"id" empty:"true"`
-	//品牌ID
+	//规格ID
 	BrandID int64 `db:"brand_id" json:"brandID" check:"id" empty:"true"`
 	//是否删除
 	IsRemove bool `json:"isRemove" check:"bool"`
@@ -22,7 +22,7 @@ type ArgsGetModelTypeList struct {
 	Search string `json:"search" check:"search" empty:"true"`
 }
 
-// GetModelTypeList 获取品牌列表
+// GetModelTypeList 获取规格列表
 func GetModelTypeList(args *ArgsGetModelTypeList) (dataList []FieldsModelType, dataCount int64, err error) {
 	dataCount, err = modelTypeDB.Select().SetFieldsList([]string{"id"}).SetFieldsSort([]string{"id", "create_at", "update_at", "delete_at", "name"}).SetPages(args.Pages).SetDeleteQuery("delete_at", args.IsRemove).SetIDQuery("org_id", args.OrgID).SetIDQuery("brand_id", args.BrandID).SetSearchQuery([]string{"name"}, args.Search).SelectList("").ResultAndCount(&dataList)
 	if err != nil || len(dataList) < 1 {
@@ -38,7 +38,7 @@ func GetModelTypeList(args *ArgsGetModelTypeList) (dataList []FieldsModelType, d
 	return
 }
 
-// GetModelType 获取品牌
+// GetModelType 获取规格
 func GetModelType(id int64, orgID int64) (data FieldsModelType) {
 	data = getModelType(id)
 	if data.ID < 1 {
@@ -51,7 +51,7 @@ func GetModelType(id int64, orgID int64) (data FieldsModelType) {
 	return
 }
 
-// GetModelTypeByCode 通过编码获取品牌
+// GetModelTypeByCode 通过编码获取规格
 func GetModelTypeByCode(code string, orgID int64) (data FieldsModelType) {
 	_ = modelTypeDB.Get().GetByCodeAndOrgID(code, orgID).Result(&data)
 	if data.ID < 1 {
@@ -64,19 +64,19 @@ func GetModelTypeByCode(code string, orgID int64) (data FieldsModelType) {
 	return
 }
 
-// ArgsCreateModelType 创建品牌参数
+// ArgsCreateModelType 创建规格参数
 type ArgsCreateModelType struct {
 	//组织ID
 	OrgID int64 `db:"org_id" json:"orgID" check:"id" empty:"true"`
-	//品牌编码
+	//规格编码
 	Code string `db:"code" json:"code" check:"des" min:"1" max:"300"`
 	//名称
 	Name string `db:"name" json:"name" check:"des" min:"1" max:"300"`
-	//品牌ID
+	//规格ID
 	BrandID int64 `db:"brand_id" json:"brandID" check:"id"`
 }
 
-// CreateModelType 创建品牌
+// CreateModelType 创建规格
 func CreateModelType(args *ArgsCreateModelType) (id int64, err error) {
 	//检查编码是否重复
 	findCodeData := GetModelTypeByCode(args.Code, args.OrgID)
@@ -98,7 +98,7 @@ func CreateModelType(args *ArgsCreateModelType) (id int64, err error) {
 	return
 }
 
-// ArgsUpdateModelType 修改品牌参数
+// ArgsUpdateModelType 修改规格参数
 type ArgsUpdateModelType struct {
 	//ID
 	ID int64 `db:"id" json:"id" check:"id"`
@@ -108,7 +108,7 @@ type ArgsUpdateModelType struct {
 	Name string `db:"name" json:"name" check:"des" min:"1" max:"300"`
 }
 
-// UpdateModelType 修改品牌
+// UpdateModelType 修改规格
 func UpdateModelType(args *ArgsUpdateModelType) (err error) {
 	//更新数据
 	err = modelTypeDB.Update().SetFields([]string{"name"}).NeedUpdateTime().AddWhereID(args.ID).AddWhereOrgID(args.OrgID).NamedExec(map[string]interface{}{
@@ -121,7 +121,7 @@ func UpdateModelType(args *ArgsUpdateModelType) (err error) {
 	return
 }
 
-// ArgsDeleteModelType 删除品牌参数
+// ArgsDeleteModelType 删除规格参数
 type ArgsDeleteModelType struct {
 	//ID
 	ID int64 `db:"id" json:"id" check:"id"`
@@ -129,7 +129,7 @@ type ArgsDeleteModelType struct {
 	OrgID int64 `db:"org_id" json:"orgID" check:"id" empty:"true"`
 }
 
-// DeleteModelType 删除品牌
+// DeleteModelType 删除规格
 func DeleteModelType(args *ArgsDeleteModelType) (err error) {
 	err = modelTypeDB.Delete().NeedSoft(true).AddWhereID(args.ID).AddWhereOrgID(args.OrgID).ExecNamed(nil)
 	if err != nil {
@@ -139,7 +139,7 @@ func DeleteModelType(args *ArgsDeleteModelType) (err error) {
 	return
 }
 
-// 获取品牌数据
+// 获取规格数据
 func getModelType(id int64) (data FieldsModelType) {
 	cacheMark := getModelTypeCacheMark(id)
 	if err := Router2SystemConfig.MainCache.GetStruct(cacheMark, &data); err == nil && data.ID > 0 {
