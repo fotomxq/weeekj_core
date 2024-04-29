8310ea989d2c3cde600ed5ad49343e9bcab20fe8
fotomxq <fotomxq@qq.com>
2024-04-29 19:49:31 +0800

    增加启动开关设计；

--- router2/system_init/install.go
+++ router2/system_init/install.go
@@ -8,6 +8,25 @@ import (
 	ToolsInstall "github.com/fotomxq/weeekj_core/v5/tools/install"
 )
 
+var (
+	//OpenInstallLocalSQL 安装本地化SQL
+	OpenInstallLocalSQL = true
+	// OpenInstallConfig 启动开关
+	OpenInstallConfig = true
+	// OpenInstallSMS 启动SMS
+	OpenInstallSMS = true
+	// OpenInstallEmail 启动邮箱
+	OpenInstallEmail = true
+	// OpenInstallUser 启动用户
+	OpenInstallUser = true
+	// OpenInstallEarlyWarning 启动预警模块
+	OpenInstallEarlyWarning = true
+	// OpenInstallFinance 启动财务
+	OpenInstallFinance = true
+	// OpenInstallOrg 启动组织
+	OpenInstallOrg = true
+)
+
 func Install() (err error) {
 	if !Router2SystemConfig.RunInstall {
 		return
@@ -16,40 +35,56 @@ func Install() (err error) {
 	ToolsInstall.Init(fmt.Sprint(CoreFile.BaseDir()+CoreFile.Sep, "install", CoreFile.Sep, "data", CoreFile.Sep))
 	//安装sql
 	Router2SystemConfig.MainDB.InstallDir = fmt.Sprint(CoreFile.BaseDir()+CoreFile.Sep, "install", CoreFile.Sep, "sql")
-	if err = Router2SystemConfig.MainDB.Install(); err != nil {
-		err = errors.New("install sql, " + err.Error())
-		return
+	if OpenInstallLocalSQL {
+		if err = Router2SystemConfig.MainDB.Install(); err != nil {
+			err = errors.New("install sql, " + err.Error())
+			return
+		}
 	}
 	//系统配置
-	if err = ToolsInstall.InstallConfig(); err != nil {
-		err = errors.New("install config, " + err.Error())
-		return
+	if OpenInstallConfig {
+		if err = ToolsInstall.InstallConfig(); err != nil {
+			err = errors.New("install config, " + err.Error())
+			return
+		}
 	}
 	//短信
-	if err = ToolsInstall.InstallSMS(); err != nil {
-		err = errors.New("install sms, " + err.Error())
-		return
+	if OpenInstallSMS {
+		if err = ToolsInstall.InstallSMS(); err != nil {
+			err = errors.New("install sms, " + err.Error())
+			return
+		}
 	}
 	//邮箱
-	if err = ToolsInstall.InstallEmail(); err != nil {
-		return errors.New("install email, " + err.Error())
+	if OpenInstallEmail {
+		if err = ToolsInstall.InstallEmail(); err != nil {
+			return errors.New("install email, " + err.Error())
+		}
 	}
 	//用户
-	if err = ToolsInstall.InstallUser(); err != nil {
-		return errors.New("install user, " + err.Error())
+	if OpenInstallUser {
+		if err = ToolsInstall.InstallUser(); err != nil {
+			return errors.New("install user, " + err.Error())
+		}
 	}
 	//预警服务
-	if err = ToolsInstall.InstallEarlyWarning(); err != nil {
-		return errors.New("install early warning, " + err.Error())
+	if OpenInstallEarlyWarning {
+		if err = ToolsInstall.InstallEarlyWarning(); err != nil {
+			return errors.New("install early warning, " + err.Error())
+		}
 	}
 	//财务
-	if err = ToolsInstall.InstallFinance(); err != nil {
-		return errors.New("install finance, " + err.Error())
+	if OpenInstallFinance {
+		if err = ToolsInstall.InstallFinance(); err != nil {
+			return errors.New("install finance, " + err.Error())
+		}
 	}
 	//组织
-	if err = ToolsInstall.InstallOrg(); err != nil {
-		err = errors.New("install org, " + err.Error())
-		return
+	if OpenInstallOrg {
+		if err = ToolsInstall.InstallOrg(); err != nil {
+			err = errors.New("install org, " + err.Error())
+			return
+		}
 	}
 	//提示信息
 	fmt.Println("main router system install success.")
