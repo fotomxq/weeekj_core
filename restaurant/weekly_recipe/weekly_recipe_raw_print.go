1b23c8905904553c68f34a85842b21463d08efef
fotomxq <fotomxq@qq.com>
2024-06-05 00:37:25 +0800

    修正菜谱excel导出缺失信息；

--- restaurant/weekly_recipe/weekly_recipe_raw_print.go
+++ restaurant/weekly_recipe/weekly_recipe_raw_print.go
@@ -3,6 +3,7 @@ package RestaurantWeeklyRecipeMarge
 import (
 	"fmt"
 	CoreFilter "github.com/fotomxq/weeekj_core/v5/core/filter"
+	OrgCore "github.com/fotomxq/weeekj_core/v5/org/core"
 	Router2Excel "github.com/fotomxq/weeekj_core/v5/router2/excel"
 	Router2Mid "github.com/fotomxq/weeekj_core/v5/router2/mid"
 )
@@ -49,6 +50,10 @@ func PrintExcelRaw(c any, logErr string, args *ArgsPrintExcelRaw) {
 	styleC4, _ := excelFile.GetCellStyle(mainSheetName, "C4")
 	styleE4, _ := excelFile.GetCellStyle(mainSheetName, "E4")
 	styleG4, _ := excelFile.GetCellStyle(mainSheetName, "G4")
+	//设置顶部信息
+	_ = excelFile.SetCellValue(mainSheetName, "B2", OrgCore.GetOrgNameByID(weeklyRecipeData.OrgID))
+	_ = excelFile.SetCellValue(mainSheetName, "E2", OrgCore.GetOrgNameByID(weeklyRecipeData.StoreID))
+	_ = excelFile.SetCellValue(mainSheetName, "G2", weeklyRecipeData.Name)
 	//当前行
 	rowIndex := 4
 	//序号
@@ -60,23 +65,33 @@ func PrintExcelRaw(c any, logErr string, args *ArgsPrintExcelRaw) {
 		_ = excelFile.SetCellValue(mainSheetName, fmt.Sprint("A", rowIndex), index)
 		//时间段
 		_ = excelFile.SetCellValue(mainSheetName, fmt.Sprint("B", rowIndex), fmt.Sprint(vItemData.DiningDate))
+		var dayType string
+		switch vItemData.DayType {
+		case 1:
+			dayType = "早餐"
+		case 2:
+			dayType = "午餐"
+		case 3:
+			dayType = "晚餐"
+		}
+		_ = excelFile.SetCellValue(mainSheetName, fmt.Sprint("C", rowIndex), fmt.Sprint(dayType))
 		//菜品
-		_ = excelFile.SetCellValue(mainSheetName, fmt.Sprint("C", rowIndex), vItemData.RecipeName)
+		_ = excelFile.SetCellValue(mainSheetName, fmt.Sprint("D", rowIndex), vItemData.RecipeName)
 		//合并单元格
-		_ = excelFile.MergeCell(mainSheetName, fmt.Sprint("C", rowIndex), fmt.Sprint("D", rowIndex))
+		_ = excelFile.MergeCell(mainSheetName, fmt.Sprint("D", rowIndex), fmt.Sprint("E", rowIndex))
 		//原材料
-		_ = excelFile.SetCellValue(mainSheetName, fmt.Sprint("E", rowIndex), vItemData.MaterialName)
+		_ = excelFile.SetCellValue(mainSheetName, fmt.Sprint("F", rowIndex), vItemData.MaterialName)
 		//合并单元格
-		_ = excelFile.MergeCell(mainSheetName, fmt.Sprint("E", rowIndex), fmt.Sprint("F", rowIndex))
+		_ = excelFile.MergeCell(mainSheetName, fmt.Sprint("F", rowIndex), fmt.Sprint("G", rowIndex))
 		//用量
-		_ = excelFile.SetCellValue(mainSheetName, fmt.Sprint("G", rowIndex), vItemData.Count)
+		_ = excelFile.SetCellValue(mainSheetName, fmt.Sprint("H", rowIndex), vItemData.Count)
 		//合并单元格
-		_ = excelFile.MergeCell(mainSheetName, fmt.Sprint("G", rowIndex), fmt.Sprint("H", rowIndex))
+		_ = excelFile.MergeCell(mainSheetName, fmt.Sprint("H", rowIndex), fmt.Sprint("I", rowIndex))
 		//设置样式
-		_ = excelFile.SetCellStyle(mainSheetName, fmt.Sprint("A", rowIndex), fmt.Sprint("B", rowIndex), styleA4)
-		_ = excelFile.SetCellStyle(mainSheetName, fmt.Sprint("C", rowIndex), fmt.Sprint("D", rowIndex), styleC4)
-		_ = excelFile.SetCellStyle(mainSheetName, fmt.Sprint("E", rowIndex), fmt.Sprint("F", rowIndex), styleE4)
-		_ = excelFile.SetCellStyle(mainSheetName, fmt.Sprint("G", rowIndex), fmt.Sprint("H", rowIndex), styleG4)
+		_ = excelFile.SetCellStyle(mainSheetName, fmt.Sprint("A", rowIndex), fmt.Sprint("C", rowIndex), styleA4)
+		_ = excelFile.SetCellStyle(mainSheetName, fmt.Sprint("D", rowIndex), fmt.Sprint("E", rowIndex), styleC4)
+		_ = excelFile.SetCellStyle(mainSheetName, fmt.Sprint("F", rowIndex), fmt.Sprint("G", rowIndex), styleE4)
+		_ = excelFile.SetCellStyle(mainSheetName, fmt.Sprint("H", rowIndex), fmt.Sprint("I", rowIndex), styleG4)
 		//行叠加
 		rowIndex += 1
 		//叠加序号
