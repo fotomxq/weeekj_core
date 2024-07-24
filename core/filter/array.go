f851402d28924ba69a44a48ddcd83dfc7bbd6137
liuyun <43500876@qq.com>
2024-07-24 15:45:00 +0800

    新增底层数组判断是否存在某个字符串

--- core/filter/array.go
+++ core/filter/array.go
@@ -9,3 +9,13 @@ func CheckInt64InArray(arr []int64, c int64) bool {
 	}
 	return false
 }
+
+// CheckStringInArray 检查string是否在列内
+func CheckStringInArray(arr []string, c string) bool {
+	for _, v := range arr {
+		if v == c {
+			return true
+		}
+	}
+	return false
+}
