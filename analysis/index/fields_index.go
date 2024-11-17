b438a73e3edadf9aec3566bf1e6006b553d74fa0
fotomxq <fotomxq@qq.com>
2024-11-17 22:12:23 +0800

    完善指标定义字段长度错误；

--- analysis/index/fields_index.go
+++ analysis/index/fields_index.go
@@ -20,7 +20,7 @@ type FieldsIndex struct {
 	// 前端应拦截内置指标的删除操作，以免影响系统正常运行，启动重启后将自动恢复，所以删除操作是无法生效的
 	IsSystem bool `db:"is_system" json:"isSystem" index:"true"`
 	//指标描述
-	Description string `db:"description" json:"description" check:"des" min:"1" max:"100" field_search:"true" field_list:"true" empty:"true"`
+	Description string `db:"description" json:"description" check:"des" min:"1" max:"-1" field_search:"true" field_list:"true" empty:"true"`
 	//指标决策建议
 	Decision string `db:"decision" json:"decision" check:"des" min:"1" max:"-1" empty:"true" field_search:"true"`
 	//指标预警阈值
