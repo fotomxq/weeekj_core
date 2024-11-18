0c66c7ea36b52b8e3ca7157be9fa0d13ace44d93
fotomxq <fotomxq@qq.com>
2024-11-19 00:57:30 +0800

    新增查看全部指标方法；

--- analysis/index/index.go
+++ analysis/index/index.go
@@ -40,6 +40,125 @@ func GetIndexList(args *ArgsGetIndexList) (dataList []FieldsIndex, dataCount int
 	return
 }
 
+// DataGetIndexAll 获取所有指标
+type DataGetIndexAll struct {
+	// ID
+	ID int64 `db:"id" json:"id" check:"id" unique:"true"`
+	//指标编码
+	Code string `db:"code" json:"code" check:"des" min:"1" max:"50" index:"true"`
+	//指标名称
+	Name string `db:"name" json:"name" check:"des" min:"1" max:"300" index:"true" field_search:"true" field_list:"true"`
+	//是否内置
+	// 前端应拦截内置指标的删除操作，以免影响系统正常运行，启动重启后将自动恢复，所以删除操作是无法生效的
+	IsSystem bool `db:"is_system" json:"isSystem" index:"true" field_list:"true"`
+	//指标描述
+	Description string `db:"description" json:"description" check:"des" min:"1" max:"-1" field_search:"true" field_list:"true" empty:"true"`
+	//指标决策建议
+	Decision string `db:"decision" json:"decision" check:"des" min:"1" max:"-1" empty:"true" field_search:"true"`
+	//指标预警阈值
+	// 0-100，归一化后的数据，超出此范围将可触发预警事件记录
+	Threshold int64 `db:"threshold" json:"threshold" index:"true"`
+	//是否启用
+	// 关闭后将不对该指标进行汇总运算
+	IsEnable bool `db:"is_enable" json:"isEnable" index:"true" field_list:"true"`
+	//子指标
+	SubIndex []DataGetIndexAll `json:"subIndex"`
+}
+
+// GetIndexAll 获取所有指标
+func GetIndexAll() (dataList []DataGetIndexAll, err error) {
+	//获取所有指标数据
+	var indexRawList []FieldsIndex
+	_, err = indexDB.GetList().GetAll(&BaseSQLTools.ArgsGetAll{
+		ConditionFields: nil,
+		IsRemove:        false,
+	}, &indexRawList)
+	if err != nil || len(indexRawList) < 1 {
+		return
+	}
+	//获取所有关系数据
+	var relationRawList []FieldsIndexRelation
+	_, _ = indexRelationDB.GetList().GetAll(&BaseSQLTools.ArgsGetAll{
+		ConditionFields: nil,
+		IsRemove:        false,
+	}, &relationRawList)
+	//如果不存在关系，则重组数据后返回
+	if len(relationRawList) < 1 {
+		for k := 0; k < len(indexRawList); k++ {
+			dataList = append(dataList, DataGetIndexAll{
+				ID:          indexRawList[k].ID,
+				Code:        indexRawList[k].Code,
+				Name:        indexRawList[k].Name,
+				IsSystem:    indexRawList[k].IsSystem,
+				Description: indexRawList[k].Description,
+				Decision:    indexRawList[k].Decision,
+				Threshold:   indexRawList[k].Threshold,
+				IsEnable:    indexRawList[k].IsEnable,
+				SubIndex:    []DataGetIndexAll{},
+			})
+		}
+		return
+	}
+	//如果存在关系
+	// 建立第一级指标
+	for _, vIndex := range indexRawList {
+		isFind := false
+		for _, vRel := range relationRawList {
+			if vRel.RelationIndexID == vIndex.ID {
+				isFind = true
+				break
+			}
+		}
+		if !isFind {
+			dataList = append(dataList, DataGetIndexAll{
+				ID:          vIndex.ID,
+				Code:        vIndex.Code,
+				Name:        vIndex.Name,
+				IsSystem:    vIndex.IsSystem,
+				Description: vIndex.Description,
+				Decision:    vIndex.Decision,
+				Threshold:   vIndex.Threshold,
+				IsEnable:    vIndex.IsEnable,
+				SubIndex:    []DataGetIndexAll{},
+			})
+		}
+	}
+	//基于第一级指标，建立下级指标
+	for k, vData := range dataList {
+		dataList[k].SubIndex = getIndexAllRelChild(indexRawList, relationRawList, vData.ID)
+	}
+	//反馈
+	return
+}
+
+// getIndexAllRelChild 无限递归构建子指标关系
+func getIndexAllRelChild(indexRawList []FieldsIndex, relationRawList []FieldsIndexRelation, indexID int64) (result []DataGetIndexAll) {
+	for _, vRel := range relationRawList {
+		if vRel.IndexID != indexID {
+			continue
+		}
+		//找到子指标，开始构建数据
+		for _, vIndex := range indexRawList {
+			if vIndex.ID != vRel.RelationIndexID {
+				continue
+			}
+			result = append(result, DataGetIndexAll{
+				ID:          vIndex.ID,
+				Code:        vIndex.Code,
+				Name:        vIndex.Name,
+				IsSystem:    vIndex.IsSystem,
+				Description: vIndex.Description,
+				Decision:    vIndex.Decision,
+				Threshold:   vIndex.Threshold,
+				IsEnable:    vIndex.IsEnable,
+				SubIndex:    getIndexAllRelChild(indexRawList, relationRawList, vIndex.ID),
+			})
+			break
+		}
+	}
+	return
+}
+
 // DataGetIndexListByTop 获取指标列表顶部数据
 type DataGetIndexListByTop struct {
 	//指标ID
@@ -92,6 +211,12 @@ func GetIndexByID(id int64) (data FieldsIndex, err error) {
 // GetIndexNameByID 获取指标名称
 func GetIndexNameByID(id int64) (name string) {
 	data, _ := GetIndexByID(id)
+	return data.Name
+}
+
+// GetIndexNameByID 获取指标名称
+func GetIndexNameByCode(code string) (name string) {
+	data, _ := GetIndexByCode(code)
 	return data.Name
 }
 
