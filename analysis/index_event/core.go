40c2cb01b5e844c9b19335118d77d081f00aa764
fotomxq <fotomxq@qq.com>
2024-11-25 12:10:49 +0800

    新增预警模块触发中间件；

--- analysis/index_event/core.go
+++ analysis/index_event/core.go
@@ -3,7 +3,10 @@ package AnalysisIndexEvent
 import BaseSQLTools "github.com/fotomxq/weeekj_core/v5/base/sql_tools"
 
 var (
+	//指标事件
 	eventDB BaseSQLTools.Quick
+	//OpenSub 是否启动订阅
+	OpenSub = false
 )
 
 func Init() (err error) {
@@ -11,5 +14,10 @@ func Init() (err error) {
 	if err = eventDB.Init("analysis_index_events", &FieldsEvent{}); err != nil {
 		return
 	}
+	if OpenSub {
+		//消息列队
+		subNats()
+	}
+	//反馈
 	return
 }
