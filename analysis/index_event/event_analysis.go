b2f8d5a2d8a9e7cd4e8dcf7553e9ce71d78dacfe
fotomxq <fotomxq@qq.com>
2024-12-02 12:41:39 +0800

    新增对风险事件的统计支持；

--- analysis/index_event/event_analysis.go
+++ analysis/index_event/event_analysis.go
@@ -14,7 +14,40 @@ type DataGetEventLevelCount struct {
 // GetEventLevelCount 获取风险等级统计
 // 该方法会无视维度，获取所有风险等级的数量统计
 func GetEventLevelCount() (dataList []DataGetEventLevelCount) {
-	_ = eventDB.GetClient().DB.GetPostgresql().Select(&dataList, "SELECT level,count(*) as count FROM "+eventDB.GetClient().TableName+" GROUP BY level")
+	_ = eventDB.GetClient().DB.GetPostgresql().Select(&dataList, "SELECT level, count(id) as count FROM "+eventDB.GetClient().TableName+" GROUP BY level")
+	return
+}
+
+// DataGetEventLevelCountByExtendOne 获取指定维度的风险事件数量关系数据
+type DataGetEventLevelCountByExtendOne struct {
+	//维度名称
+	ExtendVal string `db:"extend_val" json:"extendVal"`
+	//预警等级
+	// 根据项目需求划定等级
+	// 例如：0 低风险; 1 中风险; 2 高风险
+	Level int `db:"level" json:"level"`
+	//数量
+	Count int64 `db:"count" json:"count"`
+	//该维度下合计数量
+	AllCount int64 `db:"all_count" json:"allCount"`
+}
+
+// GetEventLevelCountByExtendOne 获取指定维度的风险事件数量关系
+func GetEventLevelCountByExtendOne(extend int) (dataList []DataGetEventLevelCountByExtendOne) {
+	extendField := fmt.Sprintf("extend%d", extend)
+	_ = eventDB.GetClient().DB.GetPostgresql().Select(&dataList, fmt.Sprintf("with r_sum as ( select %s, count(id) as count from %s group by extend2) select e.%s as extend_val, e.level as level, count(e.id), max(r.count) as all_count from %s as e, r_sum as r where e.%s != '' and e.%s = r.%s group by e.extend2, e.level order by max(r.count) desc;", extendField, eventDB.GetClient().TableName, eventDB.GetClient().TableName, extendField, extendField, extendField, extendField))
+	return
+}
+
+// GetEventLevelCountByExtendOneTop10 获取指定维度的风险事件数量关系前10
+/**
+注意，反馈的数据可能会超出10个，因为是按照组反馈的，请接口或前端根据extend合并处理
+每个extend的level至多3条，前10则最多反馈30条数据
+可能会反馈多余不相关的数据，所以需重新做合并处理
+*/
+func GetEventLevelCountByExtendOneTop10(extend int) (dataList []DataGetEventLevelCountByExtendOne) {
+	extendField := fmt.Sprintf("extend%d", extend)
+	_ = eventDB.GetClient().DB.GetPostgresql().Select(&dataList, fmt.Sprintf("with r_sum as ( select %s, count(id) as count from %s group by extend2) select e.%s as extend_val, e.level as level, count(e.id), max(r.count) as all_count from %s as e, r_sum as r where e.%s != '' and e.%s = r.%s group by e.extend2, e.level order by max(r.count) desc limit 30;", extendField, eventDB.GetClient().TableName, eventDB.GetClient().TableName, extendField, extendField, extendField, extendField))
 	return
 }
 
